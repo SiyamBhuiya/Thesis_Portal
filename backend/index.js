98d7cc0da6dc46b687b4e19937b95c0136abde04
MD Siyam Bhuiyan <siyam35-702@diu.edu.bd>
2025-06-12 13:18:56 +0600

    MongoDb Connected Succesfully

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 const express= require('express');
+const mongoose= require('mongoose');
 const app= express();
-app.get('/', (req, resp) => {
-    resp.send('Hello World!')
+const connectDB = async ()=> {
+mongoose.connect('mongodb://localhost:27017/Thesis', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB connected successfully');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
 });
+const productSchema = new mongoose.Schema({});
+const Product = mongoose.model('Product', productSchema);
+const data = await Product.find({});
+console.warn(data);
+}
+ connectDB();
+
 
 app.listen(3000);
